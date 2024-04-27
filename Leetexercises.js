51d6f1587f72fd379993f1daf9343c366ee33a39
Lukman Fashina <lukmanfashina@yahoo.com>
2024-04-27 12:01:39 -0400

    Leet

--- Leetexercises.js
+++ Leetexercises.js
@@ -1,11 +1,3 @@
-
-// https://leetcode.com/problems/two-sum/Steps• Write at least 3 example uses of the function / program, complete with output expectations
-// • Write the function signature matching the use cases
-// • Write pseudocode of your minimum intuition
-// • Turn it into code
-// • Repeat last two steps
-// • Remember to keep it simple and test the logic by reading/running your code in your head
-
 //1) https://leetcode.com/problems/score-of-a-string/description/ 
 // You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
 
